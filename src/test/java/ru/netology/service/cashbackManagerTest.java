ddab693426a58fa229cfc5b30fb879523362c319
KolominD <bogkolomind@gmail.com>
2021-11-18 00:22:42 +0300

    TestNG done

---
+++ src/test/java/ru/netology/service/cashbackManagerTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class cashbackManagerTest {
+    cashbackManager cash = new cashbackManager();
+
+    @Test
+    public void testRemain() {
+        int actual = cash.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testBoundaryBelowLimit() {
+        int actual = cash.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testBoundaryOverLimit() {
+        int actual = cash.remain(1001);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testBoundary() {
+        int actual = cash.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+}
