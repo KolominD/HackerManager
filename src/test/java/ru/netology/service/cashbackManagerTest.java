36610f2d80815cf6ad9519e3048c36a8edd16594
KolominD <bogkolomind@gmail.com>
2021-11-18 00:27:14 +0300

    junit4 done

--- src/test/java/ru/netology/service/cashbackManagerTest.java
+++ src/test/java/ru/netology/service/cashbackManagerTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class cashbackManagerTest {
     cashbackManager cash = new cashbackManager();
@@ -13,22 +13,25 @@ public class cashbackManagerTest {
         int expected = 1000;
         assertEquals(actual, expected);
     }
+
     @Test
     public void testBoundaryBelowLimit() {
         int actual = cash.remain(999);
         int expected = 1;
         assertEquals(actual, expected);
     }
+
     @Test
     public void testBoundaryOverLimit() {
         int actual = cash.remain(1001);
         int expected = 999;
         assertEquals(actual, expected);
     }
+
     @Test
     public void testBoundary() {
         int actual = cash.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
